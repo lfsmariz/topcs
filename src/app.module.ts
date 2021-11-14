32508107abf094ab47040aaddcdb08056fa91fd1
Luiz Filipe S Mariz <lfsmariz@gmail.com>
2021-11-13 22:56:14 -0300

    feat(entity): created entity article

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TopicModule } from './topic/topic.module';
+import { ArticleModule } from './article/article.module';
+import { PlayerModule } from './player/player.module';
 
 @Module({
-  imports: [],
+  imports: [TopicModule, ArticleModule, PlayerModule],
   controllers: [AppController],
   providers: [AppService],
 })
