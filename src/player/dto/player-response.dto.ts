03dee691dcf04893c62c1d8e874b0840f1d8d481
Luiz Filipe S Mariz <lfsmariz@gmail.com>
2021-11-19 14:24:23 -0300

    feat(login): created login method

---
+++ src/player/dto/player-response.dto.ts
@@ -0,0 +1,28 @@
+import { Player, PlayersArticles, PlayersTopics, Topic } from '@prisma/client';
+import { Article } from 'src/article/entities/article.entity';
+
+export class FullPlayerResponseDto {
+  id: number;
+  username: string;
+  email: string;
+  createdAt: string | Date;
+  playerTopics?: (PlayersTopics & { topic: Topic })[];
+  playerArticles?: (PlayersArticles & { article: Article })[];
+
+  constructor(
+    player: Player & {
+      playerTopics: (PlayersTopics & {
+        topic: Topic;
+      })[];
+      playerArticles: (PlayersArticles & {
+        article: Article;
+      })[];
+    },
+  ) {
+    this.username = player.username;
+    this.email = player.email;
+    this.createdAt = player.createdAt;
+    this.playerTopics = player.playerTopics;
+    this.playerArticles = player.playerArticles;
+  }
+}
